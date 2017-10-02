15c31446914ddd7b47da040a0e2c0197ddc19aae
Car0b1nius <10942878+Car0b1nius@users.noreply.github.com>
2017-10-02 23:41:42 +0200

    refactor the Python connectivity

--- pi/car_driver.py
+++ pi/car_driver.py
@@ -1,44 +1,56 @@
 import socket
 import threading
-from nav import g
-
 
 def run():
-    try:
-        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server_socket.bind(("", 3000))
-        server_socket.listen(5)
-
-        while True:
-            (client_socket, address) = server_socket.accept()
-            new_client(client_socket)
-    except IOError as e:
-        print(e)
-
+	try:
+		server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		server_socket.bind(("", 3000))
+		server_socket.listen()
+		while True:
+			(client_socket, address) = server_socket.accept()
+			new_client(client_socket)
+	except IOError as e:
+		from sys import stderr
+		print(e, file=stderr)
 
 def new_client(client_socket):
-    client_thread = threading.Thread(target=run_client, args=(client_socket,))
-    client_thread.run()
+	def run(client_socket):
+		stream = SocketStream(client_socket)
+		for op in stream:
+			instr[op](stream)
+	client_thread = threading.Thread(target=run, args=(client_socket,))
+	client_thread.run()
+	return client_thread
 
-def nop(args): pass
-def mov(args): g.outspeedcm = int.from_bytes(args(1), "big")
-def trn(args): g.steering = int.from_bytes(args(1), "big")
-def acc(args): int.from_bytes(args(1), "big") # ...
-def brk(args): raise StopIteration
+def instr(): # Scoping
+	from nav import g
+	def nop(stream): pass
+	def mov(stream): g.outspeedcm = stream(signed=True)
+	def trn(stream): g.steering =  stream(signed=True)
+	def acc(stream): bool(stream()) # Just to consume a byte for now.
+	def brk(stream): pass # For eventual deinitialization, will only be called once.
+	return {
+		0x00: nop,
+		0x01: mov,
+		0x02: trn,
+		0x03: acc,
+		0xFF: brk
+	}
+instr = instr()
 
+class SocketStream:
+	def __init__(self, client):
+		self.client = client
+		self.active=True
 
-instr = {
-    0x00: nop,
-    0x01: mov,
-    0x02: trn,
-    0x03: acc,
-    0xFF: brk,
-}
+	def __call__(self, *, size=1, signed=False, endian="big"):
+		return int.from_bytes(self.client.recv(size), endian, signed=signed)
 
+	def __next__(self):
+		if not self.active: raise StopIteration
+		r = self(signed=False)
+		if r == 0xFF: self.active = False
+		return r
 
-def run_client(client_socket):
-    try:
-        while True:
-            instruction = int.from_bytes(client_socket.recv(1), "big")
-            instr[instruction](client_socket.recv)
-    except StopIteration: pass
+	def __iter__(self):
+		return self
