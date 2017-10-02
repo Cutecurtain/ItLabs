ad9ce0fd46d51c8aa682043325701e3c9639625a
Niklas Baerveldt <niklas.baerveldt@gmail.com>
2017-10-02 13:14:24 +0200

    Changed the methods related to speedcontrol.

--- OurPythonScripts/speed_controll.py
+++ OurPythonScripts/speed_controll.py
@@ -1,16 +1,16 @@
 from nav import g
-from sensor_logic import get_filtered_sensor_values
+from sensor_logic import get_filtered_sensor_value
 from distance_logic import optimal_speed, optimal_distance
 
 def adjust_to_optimal_speed():
     
     #in cm
-    current_distance = get_filtered_sensor_values()
+    current_distance = get_filtered_sensor_value()
     
     # g.speed?? probably not right
-    new_distance = optimal_distance(g.speed)
+    #new_distance = optimal_distance(g.speed)
     
-    new_speed = optimal_speed(current_distance, new_distance)
+    new_speed = optimal_speed(current_distance)
     
     g.outputspeedcm = new_speed
         
