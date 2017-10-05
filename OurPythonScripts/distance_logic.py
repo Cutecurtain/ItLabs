e55c86cb3dd67bb3e1c232009b4287776865dfdd
Niklas Baerveldt <niklas.baerveldt@gmail.com>
2017-10-05 09:31:47 +0200

    Fixed the optimal_speed method

--- OurPythonScripts/distance_logic.py
+++ OurPythonScripts/distance_logic.py
@@ -8,5 +8,10 @@ distance_scaling = 1.1
 #	return min_dist + distance_scaling * current_speed
 
 def optimal_speed(current_distance):
-	return current_distance * 1000 - 100
+	speed = current_distance * 1000 - 100
+	if (speed > 100):
+		speed = 100
+	if (speed < -100):
+		speed = -100
+	return speed
 	
