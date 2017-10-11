2fa5bef0b1cf600daf8b1d78aee5b16f2a6046b6
Rasmus <rtmupp@gmail.com>
2017-10-11 14:41:26 +0200

    comment distance_logic.py

--- OurPythonScripts/distance_logic.py
+++ OurPythonScripts/distance_logic.py
@@ -1,12 +1,5 @@
 
-min_dist = 10 #cm
-
-#should be adjusted  
-distance_scaling = 1.1
-
-#def optimal_distance(current_speed):
-#	return min_dist + distance_scaling * current_speed
-
+#returns optimal speed based on current distance to car in front
 def optimal_speed(current_distance):
 	speed = current_distance * 240 - 100
 	if (speed > 40):
