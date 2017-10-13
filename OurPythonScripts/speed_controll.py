7ad42f0ca363d03d076315843136a95637400bba
Rasmus <rtmupp@gmail.com>
2017-10-13 11:37:00 +0200

    docstring documentation for speed_controll.py

--- OurPythonScripts/speed_controll.py
+++ OurPythonScripts/speed_controll.py
@@ -3,7 +3,7 @@ from sensor_logic import get_filtered_sensor_value
 from distance_logic import optimal_speed
 
 def adjust_to_optimal_speed():
-    """adjut car speed based on current distance and current speed"""
+    """Adjut car speed based on current distance and current speed."""
     current_distance = get_filtered_sensor_value()
     print(current_distance)
     if current_distance != None:
