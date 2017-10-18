a948f3cd6b3629cc29273721cf3268d4d49b0212
aron <aron.sandstedt@live.se>
2017-10-18 18:56:17 +0200

    copy car_driver from OurPythonScript to pi

--- pi/car_driver.py
+++ pi/car_driver.py
@@ -1,13 +1,13 @@
 import socket
 import threading
 from speed_controll import adjust_to_optimal_speed
-from camReader import main as analyse_image, image_value as iv
+from camReader import main as image_analyzer
 import time
-import math
 
 is_acc = threading.Event()
 
 def run():
+	"""Start listening for clients."""
 	try:
 		server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		server_socket.bind(("", 3000))
@@ -20,10 +20,12 @@ def run():
 		print(e, file=stderr)
 
 def new_client(client_socket):
+	"""Register a new client."""
 	def run(client_socket):
 		stream = SocketStream(client_socket)
 		for op in stream:
 			instr[op](stream)
+		instr[0xFF](stream)
 	client_thread = threading.Thread(target=run, args=(client_socket,))
 	client_thread.run()
 	return client_thread
@@ -35,6 +37,7 @@ def instr():
 	def trn(stream): g.steering = stream(signed=True)
 	def acc(stream): is_acc.set() if stream() else is_acc.clear()
 	def brk(stream):
+		print("brk")
 		is_acc.clear()
 		g.outspeedcm = 0
 	return {
@@ -53,16 +56,20 @@ def steer():
 	g.steering = steer_value
 
 def accSpeed():
+	"""Adjust speed while ACC (Adaptive Cruise Control) is enabled."""
 	while True:
 		is_acc.wait()
 		adjust_to_optimal_speed()
 		time.sleep(0.1)
 
 def accImage():
-	analyse_image(is_acc)
+	"""Take pictures with camera while ACC (Adaptive Cruise Control) is enabled."""
+	analyze_image(is_acc)
 
 def accSteer():
+	"""Adjust steering while ACC (Adaptive Cruise Control) is enabled."""
 	while True:
+		from camReader import image_value as nv
 		is_acc.wait()
 		#print("Cam_value:", str(iv))
 		if iv != None:
@@ -76,12 +83,11 @@ acc_thread_speed.start()
 acc_thread_image.start()
 acc_thread_steer.start()
 
-
-
 class SocketStream:
 	def __init__(self, client):
 		self.client = client
 		self.active = True
+		self.nop_count = 0
 
 	def __call__(self, *, size=1, signed=False, endian="big"):
 		return int.from_bytes(self.client.recv(size), endian, signed=signed)
@@ -89,7 +95,9 @@ class SocketStream:
 	def __next__(self):
 		if not self.active: raise StopIteration
 		r = self(signed=False)
-		if r == 0xFF: self.active = False
+		if r == 0xFF or self.nop_count >= 128: self.active = False
+		elif r == 0x00: self.nop_count += 1
+		else: self.nop_count = 0
 		return r
 
 	def __iter__(self):
